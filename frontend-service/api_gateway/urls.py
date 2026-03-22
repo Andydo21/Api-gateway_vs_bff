10a7fabf1f9c4da6ca81a35942192888d3dbecc2
Andydo21 <ando21445@gmail.com>
2026-03-22 22:20:01 +0700

    Final cleanup: Ensure Frontend Service is fully refactored and APISIX routes are correct

--- frontend-service/api_gateway/urls.py
+++ frontend-service/api_gateway/urls.py
@@ -6,9 +6,6 @@ from gateway.views import home, serve_frontend, serve_admin_frontend
 
 urlpatterns = [
     path('', home, name='home'),
-    path('web/', include('gateway.urls_web')),
-    path('admin-panel/', include('gateway.urls_admin')),
-    path('notifications/', include('gateway.urls_notifications')),
     path('health/', include('gateway.urls_health')),
 
     # Serve Admin UI
@@ -16,7 +13,6 @@ urlpatterns = [
     re_path(r'^admin-ui/(?P<filename>[\w\-/]+\.html)$', serve_admin_frontend, name='admin-ui-file'),
 
     # Serve frontend HTML files
-    # Truy cập: http://localhost:8000/ui/ hoặc http://localhost:8000/ui/web/index.html
     path('ui/', serve_frontend, name='frontend-home'),
     re_path(r'^ui/(?P<filename>[\w\-/]+\.html)$', serve_frontend, name='frontend-file'),
 ]
